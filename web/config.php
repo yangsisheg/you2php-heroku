2398c57fa0a99fc0df1a1262e1a63000c42232c1
yangsisheg <38121114+yangsisheg@users.noreply.github.com>
2018-04-06 11:43:19 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -5,5 +5,5 @@ define('GJ_CODE', 'KR');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'yangsisheng369@gmail.com');//邮箱
 ?>
